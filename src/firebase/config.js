1a9a6159e8697eeb0ea151d95538669b196b24f2
carlos7T <carlosagt7@gmail.com>
2020-11-27 20:18:08 -0600

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDxftBHSmPrNINT1JUJ0KbqX9MNk1aAPVM",
+    authDomain: "restaurant-ae67f.firebaseapp.com",
+    databaseURL: "https://restaurant-ae67f.firebaseio.com",
+    projectId: "restaurant-ae67f",
+    storageBucket: "restaurant-ae67f.appspot.com",
+    messagingSenderId: "856425224200",
+    appId: "1:856425224200:web:3d1202aa2b2a03c3d059bd",
+    measurementId: "G-GKE9ZL7E92"
+  };
+  export default firebaseConfig;
